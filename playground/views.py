5ce1806c5cc738b3590376f84db9976036f629d6
zhoujacob <113641223+zhoujacob@users.noreply.github.com>
2024-01-17 17:13:09 -0500

    Delete playground directory

--- playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-# request -> response
-# request handler
-
-def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Jacob'})
