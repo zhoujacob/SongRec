12bc353c0d91afe2cfe1a6adb1acc33a77fe89bb
Jacob Zhou <jacobzhou@Jacobs-MacBook-Air.local>
2024-01-17 22:15:47 -0500

    Added initial routes for future use

--- my-flask-app.py
+++ my-flask-app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! this is the home page"
+    return "Hello! this is the home page, this page will also ask what song you want to find recommendations for"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route("/recent")
+def recent_sh():
+    return "Recent Search History will be displayed here"
+
+@app.route("/saved")
+def saved_songs():
+    return "Saved songs will be displayed here"
 
 if __name__ == "__main__":
     app.run()
