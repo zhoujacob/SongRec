5b04209c690d1c55d02c2b42f0b4ce4477d68433
Jacob Zhou <jacobzhou@Jacobs-MacBook-Air.local>
2024-01-21 14:38:02 -0500

    Added flash messages for invalid submissions on the sign up page

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login", methods = ["POST", "GET"])
+@auth.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
         user = request.form["usernameID"]
@@ -14,7 +14,25 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods = ["GET", "POST"])
 def sign_up():
-    return render_template("sign-up.html")
+    if request.method == "POST":
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1') 
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category = 'error') 
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category = 'error')
+        elif password1 != password2:
+            flash('Password must match', category = 'error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters long', category = 'error') 
+        else: 
+            flash('Account created!', category = 'success')
+        return render_template("sign-up.html")
+    else:
+        return render_template("sign-up.html")
     
